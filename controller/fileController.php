ce29cd4f35699bb7fc0bf35f6f7ffc5b68ea5370
Muhammad Ridwan <muh.ridwan97@gmail.com>
2019-01-19 10:29:53 +0700

    upload

--- controller/fileController.php
+++ controller/fileController.php
@@ -35,35 +35,48 @@ class fileController{
     }
     
     public function uploadArsip($request, $response, $args) {
-        $submission_id=$request->getParsedBody();
-        $submission_id=$submission_id['submission_id'];
+        $body=$request->getParsedBody();
+        $submission_id=$body['submission_id'];
+        $uploadedFiles = $request->getUploadedFiles();
+         
+         // handle single input with single file upload
+         $uploadedFile = $uploadedFiles['fileArsip'];
+         $basename = pathinfo($uploadedFile->getClientFilename(), PATHINFO_FILENAME);
+         $namaAsli = $uploadedFile->getClientFilename();
+         $size = $uploadedFile->getSize();
+         $ekstensi = $uploadedFile->getClientMediaType();
+         //print_r($namaAsli);
         //print_r($submission_id);
+        $data = array(
+			'submission_id' => $submission_id,
+			'namaAsli' => $namaAsli,
+            'size' => $size,
+            'ekstensi' => $ekstensi
+			);	
+        
+        $userFiles = new \model\fileModel();
+        $dataUserFiles = $userFiles->setFileArsip($data);
         $path="C:\\xampp\\htdocs\\jurnal\\dataJurnal\\journals\\2\\articles\\$submission_id\\submission\productionReady";
-         //$directory ='http://localhost/jurnal/dataJurnal/journals/2/articles';
-         //$directory =__DIR__ . '\uploads';
+        
          if (!file_exists($path)) {
          mkdir($path,0777,true);
          }
          //mkdir("C:\\xampp\\htdocs\\jurnal\\dataJurnal\\journals\\2\\articles\\$submission_id\\submission\proof",0777,true);
          $directory = $path;
-         $uploadedFiles = $request->getUploadedFiles();
          
-         // handle single input with single file upload
-         $uploadedFile = $uploadedFiles['fileArsip'];
-         //print_r($uploadedFiles['fileArsip']);
-         $basename = pathinfo($uploadedFile->getClientFilename(), PATHINFO_FILENAME);
-         //print_r($directory);
+        // print_r($uploadedFiles['fileArsip']);
          
-         if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
-             $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+         
+        //  if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+        //      $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
              
-             $filename = sprintf('%s.%0.8s', $basename, $extension);
+        //      $filename = sprintf('%s.%0.8s', $basename, $extension);
      
-             $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
-             return $response->write('uploaded ' . $filename . 'berhasil <br/>');
-         }else{
-             return $response->write('gagal <br/>');
-         }
+        //      $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+        //      return $response->write('uploaded ' . $filename . 'berhasil <br/>');
+        //  }else{
+        //      return $response->write('gagal <br/>');
+        //  }
     }
     public function uploadGalley($request, $response, $args) {
         $submission_id=$request->getParsedBody();
--- model/fileModel.php
+++ model/fileModel.php
@@ -23,6 +23,23 @@
             }	
             return $data;
         }
-        
+        public function setFileArsip($data){
+            $submission_id=$data['submission_id'];
+            $namaAsli=$data['namaAsli'];
+            $size=$data['size'];
+            $ekstensi=$data['ekstensi'];
+            print_r($size);
+            $sql = "INSERT INTO submission_files(revision, submission_id, file_type, genre_id, file_size, original_file_name, file_stage, viewable, date_uploaded, date_modified, uploader_user_id, assoc_type, assoc_id) 
+            VALUES (1,10,'application/pdf',13,12312,'5-Article Text-8-1-2-20181107.pdf',10,0,'2019-01-16 05:06:48','2019-01-16 05:06:48',3,521,4)";
+
+            $stmt = $this->core->dbh->prepare($sql);
+            
+            if ($stmt->execute()) {
+                $data = $stmt->fetchAll(PDO::FETCH_OBJ);		   	
+            } else {
+                $data = 0;
+            }	
+            return $data;
+        }
     }
 ?>
